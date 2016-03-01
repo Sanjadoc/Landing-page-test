85cfd25d09ecbd972384565290b9aeaad24ec3a4
sanjadoc <sanja_doc@ukr.net>
2016-03-01 17:56:16 +0200

    LPt-06-use owl carousel for products

--- config.rb
+++ config.rb
@@ -18,3 +18,12 @@ javascripts_dir = "js"
 # line_comments = false
 
 preferred_syntax = :scss
+
+css_dir = "css" # by Compass.app 
+sass_dir = "scss" # by Compass.app 
+images_dir = "img" # by Compass.app 
+output_style = :expanded # by Compass.app 
+relative_assets = false # by Compass.app 
+line_comments = false # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
+sourcemap = false # by Compass.app 
--- js/script.js
+++ js/script.js
@@ -1,5 +1,36 @@
 $(document).ready(function() {
  
-  $("#owl-products").owlCarousel();
- 
-});
+                  $("#owl-products").owlCarousel({
+                        items : 5,
+                        itemsCustom : false,
+                        itemsDesktop : [1199,4],
+                        itemsDesktopSmall : [980,3],
+                        itemsTablet: [768,2],
+                        itemsTabletSmall: false,
+                        itemsMobile : [479,1],
+                        singleItem : false,
+                        itemsScaleUp : false,
+                      
+                      
+                        //Basic Speeds
+                        slideSpeed : 200,
+                        paginationSpeed : 800,
+                        rewindSpeed : 1000,
+
+                        //Autoplay
+                        autoPlay : true,
+                        stopOnHover : false,
+                      
+                        // Responsive 
+                        responsive: true,
+                        responsiveRefreshRate : 200,
+                        responsiveBaseWidth: window,
+                      
+                        // CSS Styles
+                        baseClass : "owl-carousel",
+                        theme : "owl-theme",
+                       
+                        //Auto height
+                        autoHeight : true,
+                  });
+                });
